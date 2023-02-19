79b439b707e5610c457da59998d1141af52a2d2d
Filip <filip.steenbergen@gmail.com>
2023-02-19 17:43:39 +0100

    Speelveld werkt helemaal

--- speelveld.py
+++ speelveld.py
@@ -9,50 +9,85 @@ def geheime_code():
     colors = ['a', 'b', 'c', 'd', 'e', 'f']
     # random keuze van 4 elementen uit 'colors'
     choice = random.sample(colors, 4)
+    # voor debuggen
+    print(choice)
 
     return choice
 
 
-print(geheime_code())
+geheime_code()
+
 
 def start_spel():
-    # deze functie moet het spel starten
-    # hoeveel je mag kiezen
-    """wat de keuze is,
+    """
+    deze functie moet het spel starten
+    hoeveel je mag kiezen
+    wat de keuze is,
     wie er speelt,
     while loop door blijven vragen
     optioneel: aantal kleuren
     """
 
-    print('Welkom bij Mastermind! Je krijgt de opties uit "a, b, c, d, e, f"')
+    print('Welkom bij Mastermind! Je krijgt de opties uit "a, b, c, d, e, f".\n'
+          'Bij dit spel is het de bedoeling de code te kraken door de juiste combinatie te maken.')
     print("Het is mogelijk om de computer het spel te laten oplossen, maar uiteraard is het leuker om zelf te spelen."
-          "Door in kleine letters 'computer' of 'speler' te typen maak je een keuze")
-    speler = input("Wie speelt er: ")
+          "Door in kleine letters 'computer' of 'mens' te typen maak je een keuze")
+    speler = input("Wie speelt er: ").lower()
     code = geheime_code()
+    vraag = ''
+    poging = 0
+    eerdere_pogingen = []
 
-    while interacties.stel_vraag(speler) != code:
-        poging = 0
-        if interacties.stel_vraag() != code:
-            poging +=1
-        elif poging == 9 and interacties.stel_vraag() != code:
-            einde_spel()
 
-def opslaan_speelveld():
+    while vraag != code:
+        vraag = interacties.stel_vraag(speler)
+        antwoord = ''
+        if vraag != code:
+            antwoord = interacties.antwoord(code, vraag)
+            poging += 1
+            opslaan_speelveld(eerdere_pogingen, poging, vraag, antwoord)
+            for p in eerdere_pogingen:
+                print(p, end='\n')
+
+        elif poging == 9 and vraag != code:
+            einde_spel(False, code)
+
+        elif vraag == code:
+            einde_spel(True, code)
+
+
+def opslaan_speelveld(eerdere_pogingen, poging, vraag, antwoord):
     """dict aanmaken om met de key de row aanmaken en de value is de ingevoerde waarde van de speler."""
-    # key = poging
+
+    eerdere_pogingen.append(f'{poging}: {vraag} : {antwoord}')
+
+    return eerdere_pogingen
+
 
 
 def update_speelveld():
+    """Eerdere pogingen tonen"""
+
+
+def einde_spel(gewonnen, code):
     """"""
+    if gewonnen:
+        print('Gefeliceerd, je hebt de geheime code geraden.')
 
+    else:
+        print(f'Helaas heb je het niet geraden in 8 pogingen. Het juiste antwoord was: {code}.')
 
-def einde_spel():
-    """"""
-    print('Helaas heb je het niet geraden in 8 pogingen. Wil je het nog een keer proberen?')
-    keuze = input('Voer hier uw keuze in: ')
+    print(f'Wil je het nog een keer proberen?')
 
-    if keuze == 'ja' or 'JA' or 'Ja':
+    keuze = input('Voer hier uw keuze in: ').lower()
+
+    if keuze == 'ja':
         start_spel()
+
     else:
         print('Fijne dag!')
-        exit()
+    exit()
+
+
+# main
+start_spel()
