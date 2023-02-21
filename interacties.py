7ebcb5bf80419c04e0638e24f1de782392eb5206
Filip <filip.steenbergen@gmail.com>
2023-02-21 22:17:51 +0100

    Uitleg toegevoegd

--- interacties.py
+++ interacties.py
@@ -17,27 +17,47 @@ def antwoord(code, stel_vraag):
 
     return f'Rood: {rood}, Wit: {wit}'
 
+def _voorgekomen_antwoord():
+    """
+    Houd een lijst bij met de al voorgekomen karakters om dubbele notaties te voorkomen.
+    :return:
+    """
+
 
 def stel_vraag(speler):
     """
+    Kijk of de input een speler of computer is.
+    Als de speler een mens is opent functie _mens_vraag().
+    Anders opent _ai_vraag()
 
     :param speler: mens, ai
     :return:
     """
 
+    # als de invoer van wie er speelt gelijk is aan 'mens',
+    # opent de functie '_mens_vraag()'.
     if speler == 'mens':
         mens = _mens_vraag()
         return mens
 
+    # als de invoer van wie er speelt gelijk is aan 'computer',
+    # opent de functie '_ai_vraag()'.
     else:
         ai = _ai_vraag()
         return ai
 
 
 def _mens_vraag():
+    """
+    Deze functie vraagt input aan de speler.
+    De input wordt gesplit op spaties.
+
+    :return: vraag.split()
+    """
+    # vraag input van de speler
     vraag = input("Voer hier een mogelijke combinatie in: ").lower().strip()
-    # hier kan al gesplit worden
-    # print(vraag)
+
+    # return de vraag met een split op de spatie
     return vraag.split(' ')
 
 def _ai_vraag():
